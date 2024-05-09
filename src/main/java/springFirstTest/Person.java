6593219846ee8cb69b9f18c471718b16026f0c71
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-05-09 20:43:12 +0200

    still playing with the basics

--- src/main/java/springFirstTest/Person.java
+++ src/main/java/springFirstTest/Person.java
@@ -1,8 +1,13 @@
 package springFirstTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
 	
 	private String name;
+	@Autowired
 	private Parrot parrot;
 	
 	public String getName() {
