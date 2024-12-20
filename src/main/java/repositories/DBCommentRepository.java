7c25c43656652ef6f1bdb3537e9ee7a7207e26bc
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-20 20:40:54 +0100

    Add spring context to project

--- src/main/java/repositories/DBCommentRepository.java
+++ src/main/java/repositories/DBCommentRepository.java
@@ -1,7 +1,10 @@
 package repositories;
 
+import org.springframework.stereotype.Component;
+
 import main.Comment;
 
+@Component
 public class DBCommentRepository implements CommentRepository {
 	
 	@Override
