ccf0a87f59477e58e3fae67fcdb273bae6a85709
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-20 21:41:07 +0100

    Use more disctinct annotations

--- src/main/java/repositories/DBCommentRepository.java
+++ src/main/java/repositories/DBCommentRepository.java
@@ -1,10 +1,11 @@
 package repositories;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import main.Comment;
 
-@Component
+@Repository
 public class DBCommentRepository implements CommentRepository {
 	
 	@Override
